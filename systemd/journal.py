fed98614dfc8166db34a73ed3598117874f23275
Nikolay Agarin <nikolay.agarin@acronis.com>
2017-12-14 17:53:54 +0300

    review fix for existing argument name

--- systemd/journal.py
+++ systemd/journal.py
@@ -146,7 +146,7 @@ class JournaldLogHandler(logging.Handler):
         args = data.pop('args', [])
         if args and len(args) == 1 and isinstance(args[0], collections.Mapping) and args[0]:
             for key, value in args[0].items():
-                key = '%s_2' % key if key in data else str(key)
+                key = 'argument_%s' % key if key in data else str(key)
                 data[key] = value
         else:
             for idx, item in enumerate(args):
