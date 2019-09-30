482e5cde2f0bbfccb035fd823267e34b42b6fc0d
Dmitry Orlov <me@mosquito.su>
2019-09-30 11:48:55 +0300

    Use pymemalloc

--- cysystemd/_journal.pyx
+++ cysystemd/_journal.pyx
@@ -1,4 +1,4 @@
-from libc.stdlib cimport malloc, free
+from cpython.mem cimport PyMem_PyMem_Malloc, PyMem_PyMem_Free
 from libc.string cimport memcpy
 from sd_journal cimport sd_journal_sendv, prioritynames, CODE, iovec
 
@@ -20,24 +20,29 @@ cpdef dict syslog_priorities():
     return result
 
 
-cdef _send(kwargs):
+cpdef _send(kwargs):
     cdef list items = list()
 
     for key, value in kwargs.items():
         key = key.upper().strip()
-        # The variable name must be in uppercase and consist only of characters,
-        # numbers and underscores, and may not begin with an underscore.
+
+        # The variable name must be in uppercase and
+        # consist only of characters, numbers and underscores,
+        # and may not begin with an underscore.
 
         if key.startswith('_'):
             raise ValueError('Key name may not begin with an underscore')
         elif not key.replace("_", '').isalnum():
-            raise ValueError('Key name must be consist only of characters, numbers and underscores')
+            raise ValueError(
+                'Key name must be consist only of characters, '
+                'numbers and underscores'
+            )
 
         items.append((key, value))
 
     cdef unsigned int count = len(items)
-    cdef iovec* vec = <iovec *>malloc(count * sizeof(iovec))
-    cdef void** cstring_list = <void **>malloc(count * sizeof(void*))
+    cdef iovec* vec = <iovec *>PyMem_Malloc(count * sizeof(iovec))
+    cdef void** cstring_list = <void **>PyMem_Malloc(count * sizeof(void*))
 
     if not vec or not cstring_list:
         raise MemoryError()
@@ -48,7 +53,7 @@ cdef _send(kwargs):
             msg = ("%s=%s\0" % (key.upper(), value)).encode()
             msg_len = len(msg)
 
-            cstring_list[idx] = <char *>malloc(msg_len)
+            cstring_list[idx] = <char *>PyMem_Malloc(msg_len)
             memcpy(cstring_list[idx], <char *> msg, msg_len)
 
             vec[idx].iov_base = cstring_list[idx]
@@ -57,10 +62,10 @@ cdef _send(kwargs):
         return sd_journal_sendv(vec, count)
     finally:
         for i in range(count):
-            free(cstring_list[i])
+            PyMem_Free(cstring_list[i])
 
-        free(cstring_list)
-        free(vec)
+        PyMem_Free(cstring_list)
+        PyMem_Free(vec)
 
 
 def send(**kwargs):
