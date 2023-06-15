f861dd0ae39b888d8436a7a963de771fee3f3857
Dmitry Orlov <me@mosquito.su>
2023-06-15 12:17:58 +0300

    add python3.11 compatibility

--- scripts/make-wheels.sh
+++ scripts/make-wheels.sh
@@ -7,11 +7,11 @@ function build_wheel() {
 	/opt/python/$1/bin/pip wheel ${SRC} -f ${SRC} -w ${DST}
 }
 
-build_wheel cp36-cp36m
 build_wheel cp37-cp37m
 build_wheel cp38-cp38
 build_wheel cp39-cp39
 build_wheel cp310-cp310
+build_wheel cp311-cp311
 
 cd ${DST}
 for f in ./*linux_*; do if [ -f $f ]; then auditwheel repair $f -w . ; rm $f; fi; done
